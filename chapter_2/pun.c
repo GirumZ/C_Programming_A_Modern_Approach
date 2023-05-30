480bd6d3dafe31639900d3c26c095e9243006164
GirumZ <girum.zebe@gmail.com>
2023-05-30 11:08:14 +0300

    Print a pun

---
+++ chapter_2/pun.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the given string
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	printf("To C, or not to C: that is the quastion.\n");
+	return (0);
+}
