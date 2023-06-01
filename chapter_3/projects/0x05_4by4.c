bb72e896a3f77fa933210e5f2cfe05b6c3ab78e6
GirumZ <girum.zebe@gmail.com>
2023-06-01 14:12:48 +0300

    four by four matrix

---
+++ chapter_3/projects/0x05_4by4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - prints a four by four matrix with row, column and diagonal sum
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p;
+
+	printf("Enter the numbers from 1 to 16 in any order: ");
+	scanf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d",
+			&a, &b, &c, &d, &e, &f, &g, &h, &i, &j, &k, &l, &m, &n, &o, &p);
+
+	printf("%d\t%d\t%d\t%d\n", a, b, c, d);
+	printf("%d\t%d\t%d\t%d\n", e, f, g, h);
+	printf("%d\t%d\t%d\t%d\n", i, j, k, l);
+	printf("%d\t%d\t%d\t%d\n", m, n, o, p);
+
+	printf("Row sums: %d %d %d %d\n", (a + b + c + d), (e + f + g + h),
+			(i + j + k + l), (m + n + o + p));
+	printf("Column sums: %d %d %d %d\n", (a + e + i + m), (b + f + j + n),
+			(c + g + k + o), (d + h + l + p));
+	printf("Diagonal sums: %d %d\n", (a + f + k + p), (m + j + g + d));
+
+	return (0);
+}
